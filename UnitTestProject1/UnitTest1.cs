010ec109b76229b30e96b86c93102519cfb1a26b
Antwan Maddox <antwan.maddox@agilethought.com>
2019-06-13 10:19:05 -0400

    commit changes

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class GoogleTest
+    {
+        IWebDriver driver;
+        [TestMethod]
+        public void OpenGoogle()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [TestCleanup]
+        public void CloseBrowser()
+        {
+            driver.Close();
+
+        }
+    }
+}
